3e38e04626d1a63cea0fd796c14a510de8d0f8f6
Paul Abramov <paul.abramov1911@gmail.com>
2022-10-19 15:11:59 +0200

    Add project files.

---
+++ CryptoTradingSystem.TestStrategy/Strategy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+using CryptoTradingSystem.General.Data;
+using CryptoTradingSystem.General.Database;
+using CryptoTradingSystem.General.Database.Models;
+using CryptoTradingSystem.General.Helper;
+using CryptoTradingSystem.General.Strategy;
+using Microsoft.Extensions.Configuration;
+
+using Serilog;
+
+namespace CryptoTradingSystem.TestStrategy
+{
+    public class Strategy : IStrategy
+    {
+        public string ExecuteStrategy(string _connectionString)
+        {
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var loggingfilePath = config.GetValue<string>("LoggingLocation");
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File(loggingfilePath, rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Information(_connectionString);
+
+            var databaseHandler = new MySQLDatabaseHandler(_connectionString);
+
+            var btcUsdt5minEma = Retry.Do(() => databaseHandler.GetIndicators<EMA>(Enums.Assets.Btcusdt, Enums.TimeFrames.M5, Enums.Indicators.EMA), TimeSpan.FromSeconds(1));
+
+            Log.Information(btcUsdt5minEma.FirstOrDefault().AssetName);
+            
+            return "test";
+        }
+    }
+}
